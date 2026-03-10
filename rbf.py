b6d82ff002cb8fa6c48d48d722b04fd3b23dccb6
shibi27 <shibistarzz@gmail.com>
2026-03-10 13:20:09 +0530

    merge 3 Python scripts into a single Colab Notebook and add sample test images

--- rbf.py
+++
@@ -1,6 +0,0 @@
-!pip install roboflow
-from roboflow import Roboflow
-rf = Roboflow(api_key="wLvpqf00Q3RZ6DYpsKFV")
-project = rf.workspace("shibi-p-xmggq").project("animal-detection-using-yolov8-cvuyo")
-version = project.version(2)
-dataset = version.download("yolov8")
